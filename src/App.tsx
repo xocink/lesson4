82606c41892384029e11b5e8ea67a9eb6fe8e84b
xocink <57539437+xocink@users.noreply.github.com>
2022-04-18 16:32:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,49 +11,7 @@ const shipmentsList : IShip[] = [
     weight: 3.4
   }
 ]
-// interface Strategy {
-//   execute(a: number, b: number): number;
-// }
-//
-// // Concrete strategies implement the algorithm while following
-// // the base strategy interface. The interface makes them
-// // interchangable in the context.
-// class ConcreteStrategyAdd implements Strategy {
-//   execute(a, b) {
-//     return a + b;
-//   }
-// }
-//
-// class ConcreteStrategySubstract implements Strategy {
-//   execute(a, b) {
-//     return a - b;
-//   }
-// }
-//
-// class ConcreteStrategyMultiply implements Strategy {
-//   execute(a, b) {
-//     return a * b;
-//   }
-// }
-//
-// class Context {
-//   private strategy: Strategy;
-//
-//   setStrategy(s: Strategy) {
-//     this.strategy = s;
-//   }
-//   // The context delegates some work to the strategy object
-//   // instead of implementing multiple versions of the
-//   // algorithm on its own.
-//   executeStrategy(a: number, b: number) {
-//     return this.strategy.execute(a, b);
-//   }
-// }
-//
-// let ctx = new Context();
-//
-// ctx.setStrategy(new ConcreteStrategyAdd());
-// ctx.executeStrategy(5, 2);
+
 export const App  = () => {
   let instance  = new Shipment(shipmentsList[0]);
   console.log(instance.ship());
